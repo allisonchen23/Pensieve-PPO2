6919157abdbd7379feed866e02df4bd398ca63bb
Allison Chen <allisonchen23@gmail.com>
2021-05-08 16:32:30 -0700

    edited test_log_folder in train.py

--- src/train.py
+++ src/train.py
@@ -23,7 +23,7 @@ RAND_RANGE = 10000
 SUMMARY_DIR = './results'
 MODEL_DIR = './models'
 TRAIN_TRACES = './cooked_traces/'
-TEST_LOG_FOLDER = './test_results/'
+TEST_LOG_FOLDER = './repeat_dense_test_results/'
 LOG_FILE = './results/log'
 PPO_TRAINING_EPO = 10
 # create result directory
